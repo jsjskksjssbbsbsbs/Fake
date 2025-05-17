6add1e86d64f8c3e3be70e583433a8be66b4229f
jsjskksjssbbsbsbs <ayanmakebot@outlook.com>
2025-05-18 00:24:29 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -83,7 +83,7 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
   }
   }
         if(event.reaction == "😠"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61564913640716"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61564913640716","61575768646052"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
